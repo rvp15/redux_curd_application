ee773929f0dde15c3e8e4b500eaf5b478e17f631
rvp15 <vedhagithub@gmail.com>
2023-09-15 10:03:30 -0400

    start over

--- redux_curd_application/src/App.js
+++ redux_curd_application/src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-   <h1>hello</h1>
-    </div>
-  );
+ 
 }
 
 export default App;
--- redux_curd_application/src/index.js
+++ redux_curd_application/src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import {configureStore} from "@reduxjs/toolkit"
+// import { Provider } from 'react-redux'; 
+
+// create a variable for the store, create a property called reducer(which contains all the reducers that we are going to use, create a folder called features and have all the reducers in there)
+// const store = configureStore({
+//   reducer:{
+//    todos: todosReducers
+//   }
+// })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Pass the store that u have created above*/}
+    
+  <App/>
+   
   </React.StrictMode>
 );
 
